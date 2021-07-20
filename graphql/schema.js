4ac7191ed3424c268d672285cce39ec90dc067c4
ronchitech <ronchibusiness@gmail.com>
2021-07-20 13:27:27 +0800

    understanding the setup and writing our first query

---
+++ graphql/schema.js
@@ -0,0 +1,16 @@
+const { buildSchema } = require('graphql');
+
+module.exports = buildSchema(`
+    type TestData {
+        text: String!
+        views: Int!
+    }
+
+    type rootQuery {
+        hello: TestData!
+    }
+
+    schema {
+        query: rootQuery
+    }
+`);
