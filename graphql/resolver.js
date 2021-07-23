fc5e35fe232a695106250b3018afb7ace34d3907
ronchitech <ronchibusiness@gmail.com>
2021-07-24 00:03:48 +0800

    fixing a bug and adding new post correctly

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -105,6 +105,7 @@ module.exports = {
     const createdPost = await post.save();
     console.log('CREATED POST', createdPost);
     user.posts.push(createdPost);
+    await user.save()
     return {
       ...createdPost._doc,
       _id: createdPost._id.toString(),
